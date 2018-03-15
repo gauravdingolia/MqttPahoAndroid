fea27eeace57b575b87c61cbea60e92c9672b78c
Gaurav Dingolia <gaurav.dingolia@gmail.com>
2018-03-15 13:59:34 +0530

    Base commit from Original Paho Mqtt Android Library + some indentation changes

---
+++ app/src/androidTest/java/org/eclipse/paho/android/service/TestCaseNotifier.java
@@ -0,0 +1,25 @@
+package org.eclipse.paho.android.service;
+
+class TestCaseNotifier {
+
+    private Throwable exception;
+
+    public void storeException(Throwable exception) {
+        this.exception = exception;
+    }
+
+    public synchronized void waitForCompletion(long timeout) throws Throwable
+    {
+
+        try {
+            wait(timeout);
+        }
+        catch (InterruptedException ignored) {}
+
+        if (exception != null) {
+            throw exception;
+        }
+
+    }
+
+}
---
+++ app/src/main/java/org/eclipse/paho/android/service/constants/Constants.java
@@ -0,0 +1,15 @@
+package org.eclipse.paho.android.service.constants;
+
+/**
+ * @author Gaurav Dingolia
+ */
+
+public class Constants
+{
+    public static final int DEFAULT_WAKELOCK_TIMEOUT_MS = 20 * 1000; //20 seconds
+
+
+    private Constants()
+    {
+    }
+}
